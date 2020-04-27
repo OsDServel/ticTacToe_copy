44a9d323b87b9bc845368115a7929a3a0de96bdb
Oscar Servellon <rockerolife66@gmail.com>
2020-04-27 14:37:21 -0700

    first commit, setting up link to gitHub to better efficiency

---
+++ python/main.py
@@ -0,0 +1,11 @@
+# input_name= input('Enter a name, in quoations')
+# # user must input a name in " "
+# # like so, 'Enter a name " money"'
+
+# output_text= 'hello'+ " "+ input_name
+# # i didnt want to keep adding a space in between the output and the input, so instead i added the space above in the code
+
+# print(output_text)
+
+import pygame
+# pygame works now via, python 3.8, and was installed by using the command "pygame==2.0.0.dev6", in the command line after installing python 3.8 and making sure to click the box that enables "python 3.8 to be added to PATH", whioch is very important because it does much of the leg work by itself. 
